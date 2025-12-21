36fd33de58f07164594ccae8eac053d874959343
Daniel Band <daniel.band@tax-academy.de>
2025-12-21 07:40:07 +0700

    solving issue #5

--- Assets/js/add-captcha.js
+++ Assets/js/add-captcha.js
@@ -1,50 +1,71 @@
-function addCaptcha(wrapper, inputName, siteKey, version, mode) {
-    if (version == 'v1') {
-        const myCustomWidget = new friendlyChallenge.WidgetInstance(wrapper, {
+(function () {
+    if (window.FriendlyCaptchaMautic) {
+        return;
+    }
+
+    function getWrapperElement(wrapperId) {
+        const wrapper = document.querySelector(`#${wrapperId}`);
+        if (!wrapper) {
+            throw new Error(`No friendly captcha wrapper element found with id '${wrapperId}'`);
+        }
+        return wrapper;
+    }
+
+    function addCaptcha(wrapper, inputName, siteKey, version, mode) {
+        if (version == 'v1') {
+            const myCustomWidget = new friendlyChallenge.WidgetInstance(wrapper, {
+                sitekey: siteKey,
+                startMode: mode,
+                solutionFieldName: inputName
+            });
+
+            return;
+        } 
+
+        const myCustomWidget = frcaptcha.createWidget({
+            element: wrapper,
             sitekey: siteKey,
             startMode: mode,
-            solutionFieldName: inputName
+            formFieldName: inputName
         });
-
-        return;
-    } 
-
-    const myCustomWidget = frcaptcha.createWidget({
-        element: wrapper,
-        sitekey: siteKey,
-        startMode: mode,
-        formFieldName: inputName
-    });
-}
-
-function checkPresenceOfFriendlyChallenge(version) {
-    return 'v1' == version && friendlyChallenge || 'v2' == version && frcaptcha;
-}
-
-/**
- * 
- * @param {string} wrapperId Friendly Captcha form wrapper id
- * @param {string} inputName Name of the friendly captcha input
- * @param {string} siteKey Friendly Captcha site key
- * @param {string} siteKey Friendly Captcha site key
- * @param {string} version Friendly Captcha api version
- */
-function scheduleAddCaptcha(wrapperId, inputName, siteKey, version, mode) {
-    const wrapper = document.querySelector(`#${wrapperId}`);
-    if (!wrapper) {
-        throw new Error(`No friendly captcha wrapper element found with id '${wrapperId}'`);
     }
 
-    if (checkPresenceOfFriendlyChallenge(version)) {
-        addCaptcha(wrapper, inputName, siteKey, version, mode)
-        return;
+    function checkPresenceOfFriendlyChallenge(version) {
+        return 'v1' == version && friendlyChallenge || 'v2' == version && frcaptcha;
     }
 
-    const timer = setInterval(function () {
+    /**
+     * @param {string} wrapperId Friendly Captcha form wrapper id
+     * @param {string} inputName Name of the friendly captcha input
+     * @param {string} siteKey Friendly Captcha site key
+     * @param {string} siteKey Friendly Captcha site key
+     * @param {string} version Friendly Captcha api version
+     */
+    function scheduleAddCaptcha(wrapperId, inputName, siteKey, version, mode) {
+        const wrapper = getWrapperElement(wrapperId);
+
         if (checkPresenceOfFriendlyChallenge(version)) {
-            addCaptcha(wrapper, inputName, siteKey, version, mode);
-            clearInterval(timer);
-        }    
-        
-    }, 5000);
-};
+            addCaptcha(wrapper, inputName, siteKey, version, mode)
+            return;
+        }
+
+        const timer = setInterval(function () {
+            if (checkPresenceOfFriendlyChallenge(version)) {
+                addCaptcha(wrapper, inputName, siteKey, version, mode);
+                clearInterval(timer);
+            }    
+            
+        }, 5000);
+    };
+
+    function displayCaptchasInForms(queue) {
+        for (const captchaData of queue) {
+            addCaptcha(getWrapperElement(captchaData.wrapperId), captchaData.inputName, captchaData.siteKey, captchaData.version, captchaData.mode);
+        }
+    }
+
+    window.FriendlyCaptchaMautic = {
+        scheduleAddCaptcha,
+        displayCaptchasInForms,
+    };
+})();
