55d531d9dcdf71ea31381116f9ef83b13ee18f5e
Daniel Band <daniel.band@tax-academy.de>
2026-03-27 09:07:58 +0700

    v5.4.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 5.4.9
+[#11](https://github.com/TAWK-GmbH/mautic-friendlycaptcha/issues/11) Implement a check if the plugin is activated before running throwing a not configured error
+
 ## 5.4.8
 Add automated tests and release workflows
--- Config/config.php
+++ Config/config.php
@@ -14,7 +14,7 @@ use MauticPlugin\MauticFriendlyCaptchaBundle\Integration\Support\ConfigSupport;
 return [
     'name'        => 'Friendly Captcha',
     'description' => 'Enables Friendly Captcha integration.',
-    'version'     => '5.4.8',
+    'version'     => '5.4.9',
     'author'      => 'Daniel Band',
     'services'    => [
         'integrations' => [
